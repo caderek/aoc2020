dfcd39890e684bf337c201214902b30c0db204f1
Maciej Caderek <maciej.caderek@gmail.com>
2020-12-16 20:56:55 +0100

    Day 16 refactor

--- src/day16/index.ts
+++ src/day16/index.ts
@@ -22,14 +22,38 @@ const prepareInput = (rawInput: string) => {
   return { rules, me, others }
 }
 
+const rowsToCols = (arr: number[][]) => {
+  const flipped = Array.from(
+    { length: arr[0].length },
+    () => new Array(arr.length),
+  )
+
+  for (let y = 0; y < arr.length; y++) {
+    for (let x = 0; x < arr[0].length; x++) {
+      flipped[x][y] = arr[y][x]
+    }
+  }
+
+  return flipped
+}
+
+const setFromRanges = (ranges: number[][]) => {
+  const set = new Set()
+
+  ranges.forEach(([from, to]) =>
+    arr.range_(from, to + 1).forEach((x) => set.add(x)),
+  )
+
+  return set
+}
+
 const goA = (rawInput: string) => {
   const { rules, others } = prepareInput(rawInput)
-  const ranges = new Set(
-    Object.values(rules)
-      .flat()
-      .map(([from, to]) => arr.range_(from, to + 1))
-      .flat(),
-  )
+  const ranges = new Set()
+
+  Object.values(rules)
+    .flat()
+    .map(([from, to]) => arr.range_(from, to + 1).forEach((x) => ranges.add(x)))
 
   let sum = 0
 
@@ -46,39 +70,19 @@ const goA = (rawInput: string) => {
 
 const goB = (rawInput: string) => {
   const { rules, others, me } = prepareInput(rawInput)
-  const ranges = new Set(
-    Object.values(rules)
-      .flat()
-      .map(([from, to]) => arr.range_(from, to + 1))
-      .flat(),
-  )
 
-  const valid = others.filter(
-    (ticket) => !ticket.some((num) => !ranges.has(num)),
-  )
+  const allowedByField = Object.values(rules).map(setFromRanges)
+  const allowed = setFromRanges(Object.values(rules).flat())
+  const valid = others.filter((nums) => nums.every((num) => allowed.has(num)))
+  const validByColumn = rowsToCols(valid)
 
-  const validCandidatesByField = Array.from(
-    { length: valid[0].length },
-    () => [],
-  )
-
-  for (let y = 0; y < valid.length; y++) {
-    for (let x = 0; x < valid[0].length; x++) {
-      validCandidatesByField[x][y] = valid[y][x]
-    }
-  }
-
-  const rangesByField = Object.values(rules).map(
-    (r) => new Set(r.map(([from, to]) => arr.range_(from, to + 1)).flat()),
-  )
-
-  const matching = Array.from({ length: rangesByField.length }, (_, i) => ({
+  const matching = Array.from({ length: allowedByField.length }, (_, i) => ({
     id: i,
     candidates: [],
   }))
 
-  rangesByField.forEach((range, i) => {
-    validCandidatesByField.forEach((candidate, j) => {
+  allowedByField.forEach((range, i) => {
+    validByColumn.forEach((candidate, j) => {
       if (candidate.every((x) => range.has(x))) {
         matching[i].candidates.push(j)
       }
@@ -103,14 +107,11 @@ const goB = (rawInput: string) => {
     .sort((a, b) => a.id - b.id)
     .map(({ result }) => result)
 
-  const rulesNames = Object.keys(rules)
-
-  const departureIndexes = arr
-    .zip_(columnNumbersByFieldId, rulesNames)
+  return arr
+    .zip_(columnNumbersByFieldId, Object.keys(rules))
     .filter(([name]) => name.startsWith("departure"))
-    .map(([_, id]) => id)
-
-  return departureIndexes.map((i) => me[i]).reduce((a, b) => a * b)
+    .map(([_, id]) => me[id])
+    .reduce((a, b) => a * b)
 }
 
 const main = async () => {
